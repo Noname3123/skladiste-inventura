f964c0a493b8d106e8f7790f84b5a77364b5463c
Benjamin Jakupovic <bjakupo@gmail.com>
2024-02-15 18:20:04 +0100

    Made page which lists all products in table format. Added link to that page in "home". If non authenticated user goes to page "proizvodi", he is redirected to login.

--- skladisteInventura/skladiste/urls.py
+++ skladisteInventura/skladiste/urls.py
@@ -4,9 +4,10 @@ URLs for app skladiste
 from django.contrib import admin
 from django.urls import path, include
 
-from skladiste.views import HomePage, SignUpView
+from skladiste.views import HomePage, SignUpView, ProizvodView
 
 urlpatterns = [
     path("", HomePage.as_view(), name="home"),
-    path("signup", SignUpView.as_view(),name="signup")
+    path("signup", SignUpView.as_view(),name="signup"),
+    path("proizvodi", ProizvodView.as_view(), name="proizvodi")
 ]
--- skladisteInventura/skladiste/views.py
+++ skladisteInventura/skladiste/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from skladiste.forms import SignUpForm
+from skladiste.models import Proizvod
 from django.urls import reverse_lazy
+from django.http import HttpResponse
 from django.views.generic.base import TemplateView
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 # Create your views here.
 
 class HomePage(TemplateView):
@@ -12,3 +14,18 @@ class SignUpView(CreateView):
     form_class =SignUpForm
     success_url=reverse_lazy("login")
     template_name="registration/signup.html"
+
+
+class ProizvodView(ListView):
+    model=Proizvod
+    template_name='proizvodList/proizvodi_list.html'
+    context_object_name='proizvodi'
+
+    def get(self, request):
+        """method called when get is called on view. It returns to home screen if user is not logged in
+        """
+        if not (request.user.is_authenticated):
+            return redirect("home")
+        
+        else:
+            return super().get(request=request)
